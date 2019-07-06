22bbb6fd5823165a7e4d9fec530f58d63dcc15ad
Rocky Fatafehi <coder97006@gmail.com>
2019-07-06 13:47:18 -0700

    pulls data from 3 different routes by using pageCounter variable to control state

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+var pageCounter = 1
 var animalContainer = document.getElementById('animal-info')
 var btn = document.getElementById('btn')
 
 btn.addEventListener('click', function() {
     var ourRequest = new XMLHttpRequest()
-    ourRequest.open('Get', 'https://learnwebcode.github.io/json-example/animals-1.json')
+    ourRequest.open('Get', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
 
 ourRequest.onload = function() {
    var ourData = JSON.parse(ourRequest.responseText)
@@ -12,13 +13,14 @@ ourRequest.onload = function() {
  
 };
 ourRequest.send()
+pageCounter++
 
 })
 
 function renderHTML(data) {
     var htmlString = ""
     for (i = 0; i < data.length; i++) {
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + "<p>"
     }
     animalContainer.insertAdjacentHTML('beforeend', htmlString)
 }
